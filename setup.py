00f7339206bd28809536c57b3a351f7c95b182da
Aijaz <aijaz523@gmail.com>
2024-07-28 21:35:32 +0530

    add

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-# from setuptools import setup, find_packages
-# from typing import List
-
-# with open('README.md', 'r', encoding='utf-8') as f:
-#     long_description = f.read()     
-   
-
-# __version__ = "0.0.4"
-# REPO_NAME = "mongodbconnectorpkg"
-# PKG_NAME= "databaseautomation"
-# AUTHOR_USER_NAME = "sunnysavita10"
-# AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
-
-# setup(
-#     name=PKG_NAME,
-#     version=__version__,
-#     author=AUTHOR_USER_NAME,
-#     author_email=AUTHOR_EMAIL,
-#     description="A python package for connecting with database.",
-#     long_description=long_description,
-#     long_description_content="text/markdown",
-#     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-#     project_urls={
-#         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-#     },
-#     package_dir={"": "src"},
-#     packages=find_packages(where="src"),
-#     )
-
-
 
 from setuptools import find_packages, setup
 from typing import List
@@ -49,20 +19,6 @@ package_dir={"":"src"}
 with open("README.md", 'r', encoding='utf-8') as f:
     long_description=f.read()
 
-"""
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    try:
-        with open(file_path) as file_obj:
-            requirements=file_obj.readlines()
-            requirements=[req.replace("\n","") for req in requirements]
-            if HYPEN_E_DOT in requirements:
-                requirements.remove(HYPEN_E_DOT)
-    except FileNotFoundError:
-        print(f"Warning: {file_path} not found.")
-        return []
-    return requirements
-"""
 
 setup(
     name=PKG_NAME,
@@ -76,8 +32,4 @@ setup(
     project_urls=project_urls,
     package_dir=package_dir,
     packages=find_packages(where="src"),
-    # install_requires=get_requirements("requirements_dev.txt")
 )
-
-# if __name__ == "__main__":
-#     get_requirements("requirements_dev.txt")
